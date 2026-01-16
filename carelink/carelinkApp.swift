373df0731353dc545053345ad2749b0ff8fa38a5
kalipeng <kellypear01@gmail.com>
2026-01-15 20:08:09 -0800

    Initial Commit

---
+++ carelink/carelinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  carelinkApp.swift
+//  carelink
+//
+//  Created by kellypeng on 1/15/26.
+//
+
+import SwiftUI
+
+@main
+struct carelinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
